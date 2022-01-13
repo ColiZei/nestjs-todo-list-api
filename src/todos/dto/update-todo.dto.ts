9aecf46abd714f667d7e1a7ec97c6565ab02304e
Sebastian Collazo-Quaas <mission@collazo.de>
2022-01-13 21:47:36 +0100

    Update dtos

--- src/todos/dto/update-todo.dto.ts
+++ src/todos/dto/update-todo.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateTodoDto } from './create-todo.dto';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {
+  @ApiProperty()
+  id: number;
+}
