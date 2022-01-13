ae4b7973ce862ee4f307bd7f3f0aeb6aad065ad9
Sebastian Collazo-Quaas <mission@collazo.de>
2022-01-13 22:08:49 +0100

    Added first controller tests

--- src/todos/todos.controller.spec.ts
+++ src/todos/todos.controller.spec.ts
@@ -1,23 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { Todo } from './entities/todo.entity';
+import { TodoRepository } from './entities/todo.repository';
+
 import { TodosController } from './todos.controller';
 import { TodosService } from './todos.service';
 
+const mockRepository = {};
+
 describe('TodosController', () => {
   let controller: TodosController;
+  let service: TodosService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TypeOrmModule.forFeature([Todo])],
       controllers: [TodosController],
-      providers: [TodosService],
+      providers: [
+        TodosService,
+        {
+          provide: getRepositoryToken(TodoRepository),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<TodosController>(TodosController);
+    service = module.get<TodosService>(TodosService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of todos', async () => {
+      const result: Todo[] = [{ id: 1, description: 'Test', status: false }];
+
+      jest
+        .spyOn(service, 'findAll')
+        .mockImplementation(() => Promise.resolve(result));
+
+      expect(await controller.findAll()).toBe(result);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return an array of todos', async () => {
+      const result: Todo = { id: 2, description: 'Test', status: false };
+
+      jest
+        .spyOn(service, 'findOne')
+        .mockImplementation(() => Promise.resolve(result));
+
+      expect(await controller.findOne('2')).toBe(result);
+    });
   });
 });
--- src/todos/todos.service.spec.ts
+++ src/todos/todos.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TodoRepository } from './entities/todo.repository';
 import { TodosService } from './todos.service';
+
+const mockRepository = {};
 
 describe('TodosService', () => {
   let service: TodosService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodosService],
+      providers: [
+        TodosService,
+        {
+          provide: getRepositoryToken(TodoRepository),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<TodosService>(TodosService);
